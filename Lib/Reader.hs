241afa50adbaec4dced906ee5204835635326fb8
CharlesLB <charles.braga.jf.cb@gmail.com>
2024-05-18 18:15:34 -0300

    ref: getUserBotLevel

--- Lib/Reader.hs
+++ Lib/Reader.hs
@@ -3,19 +3,40 @@ module Lib.Reader (getUserBotLevel, displayPossibleRotations) where
 import Core.Board.Dice (Dice (..), possibleRotations)
 import Data.Char (toLower)
 import Lib.Printer (displayBotLevels, printDiceConfiguration)
+import Text.Read (readMaybe)
 import Types.BotLevel (BotLevel (..))
+
+readInt :: Maybe Int -> Maybe Int -> IO Int
+readInt Nothing Nothing = do
+  input <- getLine
+  maybe invalidInput return (readMaybe input)
+  where
+    invalidInput = do
+      putStrLn "Input inválido. Tente novamente."
+      readInt Nothing Nothing
+readInt minBound maxBound = do
+  input <- getLine
+  case readMaybe input of
+    Just n -> case (minBound, maxBound) of
+      (Just minVal, Just maxVal) | n >= minVal && n <= maxVal -> return n
+      (Just minVal, Nothing) | n >= minVal -> return n
+      (Nothing, Just maxVal) | n <= maxVal -> return n
+      _ -> invalidInput
+    Nothing -> invalidInput
+  where
+    invalidInput = do
+      putStrLn "Input inválido. Selecione uma opção válida."
+      readInt minBound maxBound
 
 getUserBotLevel :: IO BotLevel
 getUserBotLevel = do
   displayBotLevels
   putStrLn "Digite o número correspondente ao nível desejado:"
-  choice <- getLine
-  case map toLower choice of
-    "1" -> return Easy
-    "2" -> return Hard
-    _ -> do
-      putStrLn "Opção inválida. Por favor, escolha uma opção válida (1 ou 2)."
-      getUserBotLevel
+  choice <- readInt (Just 1) (Just 2)
+  case choice of
+    1 -> return Easy
+    2 -> return Hard
+    _ -> getUserBotLevel
 
 displayPossibleRotations :: Dice -> IO Int
 displayPossibleRotations chosenDice = do
