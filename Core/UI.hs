c87461a04a6e194ca8e057546847ecd91a562a72
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2024-05-18 10:39:02 -0300

    fix: Mudando exibição para girar somente quando tiver != 1

--- Core/UI.hs
+++ Core/UI.hs
@@ -1,43 +1,29 @@
-module Core.UI (initializingDices, initializingHumanPlayer, initializingBotPlayer, getPlayerMove) where
+module Core.UI (getNumberOfDices, getNameHumanPlayer, getLevelBoyPlayer, getPlayerMove) where
 
 import Control.Monad.State
-import Core.Dice (Dice (..), initializeDices)
-import Core.Players.BotPlayer (BotPlayer (..), initializeBotPlayer)
-import Core.Players.HumanPlayer (HumanPlayer (..), initializeHumanPlayer)
+import Core.Dice (Dice (..))
+import Core.Players.BotPlayer (BotLevel)
+import Core.Players.HumanPlayer (HumanPlayer (..))
 import Core.Players.Player (Player (..), PlayerType (..))
 import Lib.Printer (printDiceConfiguration)
 import Lib.Reader (displayPossibleRotations, getUserBotLevel)
 
-initializingDices :: IO [Dice]
-initializingDices = do
+getNumberOfDices :: IO Int
+getNumberOfDices = do
   putStrLn "Quantos dados deseja jogar? "
   numDice <- readLn
-  --   Mover initializeDices para o game e renomear essa função para getNumberOfDices
-  dices <- initializeDices numDice
-  printDiceConfiguration dices
+  return numDice
 
-  return dices
-
-initializingHumanPlayer :: IO HumanPlayer
-initializingHumanPlayer = do
+getNameHumanPlayer :: IO String
+getNameHumanPlayer = do
   putStrLn "Qual o nome do jogador? "
   nameHumanPlayer <- getLine
-  human <- initializeHumanPlayer nameHumanPlayer
-  --   Mover initializeHumanPlayer para o game e renomear essa função para getHumanData
-  putStrLn $ "O nome do jogador do tipo " ++ show (playerType human) ++ " é: " ++ playerName human
-  return human
+  return nameHumanPlayer
 
-initializingBotPlayer :: IO BotPlayer
-initializingBotPlayer = do
-  putStrLn "Qual o nome do bot? "
-  nameBotPlayer <- getLine
+getLevelBoyPlayer :: IO BotLevel
+getLevelBoyPlayer = do
   levelBotPlayer <- getUserBotLevel
-  --   Mover initializeBotPlayer para o game e renomear essa função para getBotData
-  bot <- initializeBotPlayer nameBotPlayer levelBotPlayer
-
-  putStrLn $ "O nome do jogador do tipo " ++ show (playerType bot) ++ " é: " ++ playerName bot ++ ". Ele é do nivel " ++ show (botLevel bot)
-
-  return bot
+  return levelBotPlayer
 
 -- TODO mover lógica do jogo para o Game
 possibleDicesToRemovals :: [Dice] -> [(Int, Int)]
@@ -57,7 +43,10 @@ getPlayerMove diceList = do
   putStrLn "Escolha a jogada a ser feita:"
 
   -- TODO fazer verificação se posso girar o dado
-  putStrLn "1. Girar"
+  if any (\dice -> value dice /= 1) diceList
+    then putStrLn "1. Girar"
+    else return ()
+
 
   if any (\dice -> value dice == 1) diceList
     then putStrLn "2. Retirar"
@@ -66,22 +55,25 @@ getPlayerMove diceList = do
   putStrLn "Digite o número correspondente à ação desejada:"
   choicePlayer <- readLn
   case choicePlayer of
-    1 -> do
-      let dicesToRotations = possibleDicesToRotations diceList
-      putStrLn "Possíveis rotações disponíveis:"
-      mapM_ (\(i, option) -> putStrLn $ "Dado " ++ show i ++ ": " ++ show option) dicesToRotations
-      -- let numDicesToRotations = length dicesToRotations
-      -- putStrLn $ "Número de opções disponíveis para girar: " ++ show numDicesToRotations
-      putStrLn "Escolha o dado para girar:"
-      index <- readLn
-      if any (\(i, option) -> i == index) dicesToRotations
+    1 -> 
+      if any (\dice -> value dice /= 1) diceList
         then do
-          let chosenDice = diceList !! (index - 1)
-          putStrLn $ "Dado escolhido: " ++ show (value chosenDice)
-          newValue <- displayPossibleRotations chosenDice
-          return (1, index, newValue)
+          let dicesToRotations = possibleDicesToRotations diceList
+          putStrLn "Possíveis rotações disponíveis:"
+          mapM_ (\(i, option) -> putStrLn $ "Dado " ++ show i ++ ": " ++ show option) dicesToRotations
+          putStrLn "Escolha o dado para girar:"
+          index <- readLn
+          if any (\(i, option) -> i == index) dicesToRotations
+            then do
+              let chosenDice = diceList !! (index - 1)
+              putStrLn $ "Dado escolhido: " ++ show (value chosenDice)
+              newValue <- displayPossibleRotations chosenDice
+              return (1, index, newValue)
+            else do
+              putStrLn "Índice inválido. Tente novamente."
+              getPlayerMove diceList
         else do
-          putStrLn "Índice inválido. Tente novamente."
+          putStrLn "Não há dados sem face 1 para girar."
           getPlayerMove diceList
     2 ->
       if any (\dice -> value dice == 1) diceList
