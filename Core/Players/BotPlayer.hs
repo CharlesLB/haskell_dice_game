e7e0c04eb5658e760c44775e33cd4ad62544bb9d
CharlesLB <charles.braga.jf.cb@gmail.com>
2024-05-19 19:47:00 -0300

    feat: add hardBotMove

--- Core/Players/BotPlayer.hs
+++ Core/Players/BotPlayer.hs
@@ -2,6 +2,7 @@ module Core.Players.BotPlayer (BotPlayer (..), BotLevel (..), initializeBotPlaye
 
 import Core.Board.Board (Board)
 import Core.Board.Dice (Dice (..), possibleRotations)
+import Data.List (delete)
 import System.Random (randomRIO)
 import Types.BotLevel (BotLevel (..))
 import Types.Move (Move (..))
@@ -21,7 +22,7 @@ botPlay :: BotPlayer -> Board -> IO Move
 botPlay player gameState = do
   case botLevel player of
     Easy -> easyBotMove gameState
-    Hard -> easyBotMove gameState
+    Hard -> hardBotMove gameState
 
 easyBotMove :: Board -> IO Move
 easyBotMove board = do
@@ -36,3 +37,40 @@ easyBotMove board = do
       return (UpdateMove {updateIndex = randomIndex, newValue = newValue})
     else do
       return (RemoveMove {removeIndex = randomIndex})
+
+hardBotMove :: Board -> IO Move
+hardBotMove board = do
+  let potentialMoves = [(i, r) | i <- [0 .. length board - 1], r <- possibleRotations (board !! i)]
+  let winningMoves = [(i, r) | (i, r) <- potentialMoves, isLoser (updateBoard board i r)]
+  let removeMoves = [i | i <- [0 .. length board - 1], value (board !! i) == 1]
+
+  case (winningMoves, removeMoves) of
+    ((i, r) : _, _) -> return $ UpdateMove {updateIndex = i, newValue = r}
+    (_, i : _) -> return $ RemoveMove {removeIndex = i}
+    _ -> easyBotMove board
+  where
+    isWinner :: Board -> Bool
+    isWinner board = not (isLoser board)
+
+    isLoser :: Board -> Bool
+    isLoser board = case board of
+      [] -> False
+      [dice] -> value dice `elem` [2, 5]
+      [dice1, dice2]
+        | value dice1 == value dice2 -> True
+        | value dice1 + value dice2 == 7 -> True
+        | otherwise -> False
+      _ -> all (\dice -> value dice == 2 || value dice == 5) board || checkPairs board
+
+    checkPairs :: Board -> Bool
+    checkPairs [] = True
+    checkPairs [x] = False
+    checkPairs (x : xs) = any (checkPair x xs) (zip [0 ..] xs) || checkPairs xs
+
+    checkPair :: Dice -> Board -> (Int, Dice) -> Bool
+    checkPair x xs (i, y) =
+      isLoser [x, y] && isLoser (take i xs ++ drop (i + 1) xs)
+
+    updateBoard :: Board -> Int -> Int -> Board
+    updateBoard board index newValue =
+      take index board ++ [Dice {value = newValue}] ++ drop (index + 1) board
